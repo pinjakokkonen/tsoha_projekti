c43559b5b1adeefab75791867f8265fda6d2afbd
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2024-04-13 17:31:02 +0300

    Second version, log in, log out and creating an account

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask import redirect, render_template, request, session
 from os import getenv
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///user"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -14,19 +19,42 @@ def index():
 def form():
     return render_template("form.html")
 
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
 @app.route("/login",methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
     # TODO: check username and password
-    session["username"] = username
-    return redirect("/")
+    sql = text("SELECT id, password FROM users WHERE username=:username")
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()    
+    if not user:
+        # TODO: invalid username
+        pass
+    else:
+        hash_value = user.password
+        if check_password_hash(hash_value, password):
+            # TODO: correct username and password
+            session["username"] = username
+        else:
+            # TODO: invalid password
+            pass
+    return redirect("/form")
 
 @app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
 
-# @app.route("/result", methods=["POST"])
-# def result():
-#     return render_template("result.html", name=request.form["name"])
+@app.route("/create_account",methods=["POST"])
+def create_account():
+    username = request.form["username"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
+    db.session.execute(sql, {"username":username, "password":hash_value})
+    db.session.commit()
+    return redirect("/")
